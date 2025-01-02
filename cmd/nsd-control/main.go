3801b36b98e5a9db081c33bfc8d922d5fbab3636
Jener Rasmussen <git@jener.me>
2025-01-02 20:52:58 +0100

    TLS transport

--- cmd/nsd-control/main.go
+++ cmd/nsd-control/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"nsd/pkg/client"
+	"os"
+	"strings"
 )
 
 const defaultSocket = "/var/run/nsd.sock"
 
+type SimpleAddr string
+
+func (s SimpleAddr) Network() string {
+	return "tcp"
+}
+
+func (s SimpleAddr) String() string {
+	if !strings.Contains(string(s), ":") {
+		return string(s) + ":8952"
+	}
+	return string(s)
+}
+
 func main() {
-	addr := flag.String("i", defaultSocket, "server address and port, or socket path")
+	connUrl := flag.String("i", defaultSocket, "server address and port, or socket path")
+	caPath := flag.String("ca", "", "TODO")
+	clientCertPath := flag.String("client-cert", "", "TODO")
+	clientKeyPath := flag.String("client-key", "", "TODO")
 	flag.Parse()
 	posArgs := flag.Args()
 
@@ -19,19 +40,62 @@ func main() {
 		return
 	}
 
-	// TODO check if we got an IP
-	c, err := client.NewUNIXSocketClient(*addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(c *client.Client) {
-		err := c.Close()
+	var c *client.Client
+	if _, err := os.Stat(*connUrl); err == nil {
+		c, err = client.NewUNIXSocketClient(*connUrl)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
-	}(c)
+	} else {
+		// CA that signed the server certificate
+		if caPath == nil || *caPath == "" {
+			log.Fatal("missing -ca")
+			return
+		}
+		caCert, err := os.ReadFile(*caPath)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		caPool := x509.NewCertPool()
+		caPool.AppendCertsFromPEM(caCert)
 
-	switch posArgs[0] {
+		// Client certificate
+		if clientCertPath == nil || *clientCertPath == "" {
+			log.Fatal("missing -client-cert")
+			return
+		}
+		if clientKeyPath == nil || *clientKeyPath == "" {
+			log.Fatal("missing -client-key")
+			return
+		}
+		clientCert, err := tls.LoadX509KeyPair(*clientCertPath, *clientKeyPath)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		c, err = client.NewSimpleTLSClient(SimpleAddr(*connUrl), caPool, clientCert)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+
+	defer mustClose(c)
+
+	doCommand(c, posArgs[0])
+}
+
+func mustClose(c io.Closer) {
+	err := c.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func doCommand(c *client.Client, cmd string) {
+	switch cmd {
 	case "stop":
 		if err := c.Stop(); err != nil {
 			log.Fatal(err)
--- pkg/client/tls.go
+++ pkg/client/tls.go
@@ -1,1 +1,42 @@
 package client
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"net"
+)
+
+func NewSimpleTLSClient(addr net.Addr, serverCA *x509.CertPool, clientCert tls.Certificate) (*Client, error) {
+	tlsConfig := &tls.Config{
+		RootCAs:      serverCA,
+		Certificates: []tls.Certificate{clientCert},
+	}
+	conn, err := tls.Dial(addr.Network(), addr.String(), tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client{
+		socket: conn,
+	}
+	if err := client.init(); err != nil {
+		return nil, err
+	}
+	return client, err
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func NewTLSClient(addr net.TCPAddr, tlsConfig *tls.Config) (*Client, error) {
+	conn, err := tls.Dial(addr.Network(), addr.String(), tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client{
+		socket: conn,
+	}
+	if err := client.init(); err != nil {
+		return nil, err
+	}
+	return client, err
+}
